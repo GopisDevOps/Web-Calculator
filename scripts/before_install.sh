01952ed405954c6892f21fb8333bc06ec1deae58
Gopinath R <gopinath.rs@gmail.com>
2024-02-08 18:49:25 +0000

    Edited before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 rm -rf /var/www/html/*
+
+# Download index.html from S3 bucket
+aws s3 cp s3://web-calculator-gopi/webcalculator/index.html /var/www/html/
+aws s3 cp s3://web-calculator-gopi/webcalculator/script.js /var/www/html/
+aws s3 cp s3://web-calculator-gopi/webcalculator/styles.css /var/www/html/
